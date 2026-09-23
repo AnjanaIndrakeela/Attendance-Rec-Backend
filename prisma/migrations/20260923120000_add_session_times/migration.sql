b396e2af20d56d9d2c13eb68b8b341c2d35afbab
D.M.A.I. Disanayaka <anjanaindrakeelauni@gmail.com>
2026-09-23 21:41:43 +0530

    Updated Overview and Roster Pages

---
+++ prisma/migrations/20260923120000_add_session_times/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN "startTime" TIMESTAMP(3),
+ADD COLUMN "endTime" TIMESTAMP(3);
---
+++ prisma/migrations/20260923130000_add_player_profile_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Player" ADD COLUMN "position" TEXT,
+ADD COLUMN "birthday" TIMESTAMP(3),
+ADD COLUMN "batch" TEXT,
+ADD COLUMN "department" TEXT,
+ADD COLUMN "faculty" TEXT;
